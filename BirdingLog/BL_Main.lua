fea5a9dca9f92bbc267339213f16baecdf118dad
Billy Van Ark <4hchm4xub2@snkmail.com>
2024-09-18 21:59:15 +0200

    Release 0.9.4 (2024-08-19)

---
+++ BirdingLog/BL_Main.lua
@@ -0,0 +1,283 @@
+-- Birding Log by David Down
+-- coding: utf-8 '�
+
+import "Turbine.Gameplay"
+import "Turbine.UI.Lotro"
+import "Vinny.Common.EII_ID"
+import "Vinny.Common.Sort"
+import "Vinny.BirdingLog.BL_Data"
+
+function print(text) Turbine.Shell.WriteLine("<rgb=#00FFFF>BL:</rgb> "..tostring(text)) end
+function printh(text) print("<rgb=#00FF00>"..text.."</rgb>") end
+function printe(text) print("<rgb=#FF6040>Error: "..text.."</rgb>") end
+
+import "Vinny.Common.Help"
+
+local locPat = "You are on %a* server %d* at r(%d) lx(%d+) ly(%d+) ox(.-%d+%.?%d*) oy(.-%d+%.?%d*) oz(.-%d+%.?%d*)"
+local liPat = "You are on %a* server %d* at r(%d) lx(%d+) ly(%d+) i%d* ox(.-%d+%.?%d*) oy(.-%d+%.?%d*) oz(.-%d+%.?%d*)"
+local iPat = "You are on %a* server %d* at r(%d) lx(%d+) ly(%d+) cInside ox(.-%d+%.?%d*) oy(.-%d+%.?%d*) oz(.-%d+%.?%d*)"
+local xlink = "<Examine:IIDDID:0x0000000000000000:0x700%s>[%s]<\\Examine>"
+local xpat = "<Examine:IIDDID:0x0%x+:0x700(%x+)>%[(.-)%]<\\Examine>"
+local fpPat = "Your proficiency in Birding has increased to (%d+)."
+local nPat = "(%d+) (.+)"
+local Zloc = "^(.+): (.+): (%d+%.%d[NS]), (%d+%.%d[EW])$"
+local x0,y0 = 1468,1244
+locStr = false
+Track = false
+-- You have acquired: [Minnow].
+-- Your proficiency in Birding has increased to 9.
+
+Plugins.BirdingLog.Open = function(sender,args)
+	BL_window:SetVisible( true )
+	BL_window:SetZOrder( 2 )
+end
+
+BL_Options = Turbine.PluginData.Load(Turbine.DataScope.Server,"BL_Options")
+if not BL_Options then BL_Options = {} end
+
+local FLv = "Birding Log "..Plugins["BirdingLog"]:GetVersion()
+
+Locs = Turbine.PluginData.Load(Turbine.DataScope.Server,"BL_Locs")
+if type(Locs) == "table" then
+	if next(Locs) and #next(Locs) > 2 then
+		print("Converting Locs to Zones..")
+		local zt = {}
+		for z in pairs(Zone) do zt[z] = {} end
+		for l,t in pairs(Locs) do
+			z = zt[t.z]
+			for id,n in pairs(t) do
+				if #id==5 then z[id] = (z[id] or 0)+n end
+			end
+		end
+		Locs = zt
+	end
+else Locs = {} end
+
+Totals = Turbine.PluginData.Load(Turbine.DataScope.Character,"BL_Totals")
+if type(Totals) ~= "table" then 
+	Totals = {} 
+	print("Created new birding record")
+end
+
+import "Vinny.BirdingLog.BL_Window"
+
+-- Save player name for later use
+player = Turbine.Gameplay.LocalPlayer.GetInstance()
+pname = player:GetName()
+
+printh(FLv..", data loaded.")
+
+function pos(n0,ls,os)
+	return (tostring(ls) +math.fmod(tostring(os),20)/20 -n0)/10
+end
+
+Turbine.Chat.Received = function (sender,args)
+	local msg = args.Message
+	if not msg then return end
+	local fp = msg:match(fpPat)
+	if fp then Totals.fp = fp return end
+--	print("Chat type="..args.ChatType)
+	if args.ChatType==Turbine.ChatType.SelfLoot then 
+		if msg:find("You have acquired: ") then
+			local id,name = msg:match(xpat)
+			if not id then id,name = Vinny.Common.EII_ID(msg) end
+			if not id then return end
+			if name:sub(-5)=="Frame" then return end
+			if ID[id] then
+				if not Totals[id] then
+					Totals[id] = 0
+					print("New type of bird found.")
+				end
+				Totals[id] = Totals[id]+1
+				print("Saw a "..ID[id].n..", count="..Totals[id])
+				if locStr then
+					local locTbl = Locs[locStr]
+					if not locTbl then return end
+					if not locTbl[id] then locTbl[id] = 0 end
+					locTbl[id] = locTbl[id]+1
+				end
+			elseif Track then 
+				printe("Unknown: "..name..", id="..tostring(id)) 
+			end
+		end
+		return
+	end
+	if args.ChatType~=Turbine.ChatType.Standard then return end
+	local tt = msg:match("^Taking the contents of the (.+)....$")
+	if tt then last = last==tt return end
+	local r,lx,ly,ox,oy,oz = msg:match(locPat)
+	if not r then r,lx,ly,ox,oy,oz = msg:match(liPat) end
+	if not r then r,lx,ly,ox,oy,oz = msg:match(iPat) end
+	if not r then return end
+	local ew,ns = pos(x0,lx,ox), pos(y0,ly,oy)
+	locStr = string.format("%.1f,%.1f",ns,ew)
+	print("r="..r..", ns="..ns..", ew="..ew.."  ("..locStr..")")
+end
+
+local function distance(dy,dx) return math.sqrt(dy*dy+dx*dx) end
+
+local function locV(str,neg)
+    local nbr = tonumber(str:sub(1,-2))
+    if str:sub(-1)==neg then nbr = -nbr end
+    return nbr
+end
+
+local function print_list(list)
+	local nr,t = 0,{}
+	for id,n in pairs(list) do
+		if #id==5 then
+			table.insert(t,id)
+		end
+	end
+	table.sort(t, function(a,b) return ID[a].n<ID[b].n end )
+	for i,id in ipairs(t) do
+		local t,n = ID[id],list[id]
+		local s = string.format(xlink,id,t.n)
+		print(s..': '..n)
+		nr = nr + n
+	end
+	print("Total sighting count: "..nr)
+	return
+end
+
+BL_Command = Turbine.ShellCommand()
+function BL_Command:GetShortHelp() return Vinny.Common.Help(help,"??") end
+function BL_Command:GetHelp() return Vinny.Common.Help(help,"help") end
+
+function BL_Command:Execute( cmd,args )
+	if Vinny.Common.HelpCmd(cmd,args,help) then return end
+    if cmd=="bll" then
+		if args=="list" then
+			if locStr then
+				printh("Birds sighted in "..Zone[locStr].z)
+				print_list(Locs[locStr])
+			else printe("No zone selected yet") end
+			return
+		end
+		if args=="zone" then
+			if locStr then
+				local z = Zone[locStr]
+				printh("Birds to see in "..z.z)
+				for id in Sort(z.id,function(a,b) return ID[a].n<ID[b].n end) do
+					local t,n = ID[id], Totals[id] or 0
+					local s = string.format(xlink,id,t.n)
+					print(s..': '..n)
+				end
+			else printe("No zone selected yet") end
+			return
+		end
+		local reg,a,y,x = args:match(Zloc)
+		if y then
+			local r = Region[reg]
+			if not r then printe("Unknown region: "..reg) return end
+			if r~=1 then printe("Birding requires Eriador.") return end
+			local y1,x1,ln = locV(y,'S'), locV(x,'W')
+			local loc = r..';'..y1..','..x1
+			locStr = nil
+			local zc,zn
+			for c,t in pairs(Zone) do
+				if y1<t.n and y1>t.s and x1<t.e and x1>t.w then
+					zc = c; zn = t.z break end
+			end
+			if zn then 
+				print("Zone: "..zn)
+				locStr = zc
+				if not Locs[zc] then Locs[zc] = {} end
+				BL_window.zoneMenu:SetText( Zone[zc].z )
+			else printe("Zone not found.") end
+		else printe("Invalid location for Birding.") end
+		return
+	end
+	if args=="show" or cmd=="blw" then
+		BL_window:SetVisible( true )
+		return
+	end
+    if args=="sight" then
+		printh("Birding sighting record:")
+		print_list(Totals)
+		return
+	end
+	if args=="zones" then
+		printh("Birds found by zone:")
+		for zc,zt in Sort(Zone) do
+			local zn,pn = 0,0
+			for id in pairs(zt.id) do
+				zn = zn+1
+				if Totals[id] then pn = pn+1 end
+			end
+			print(zt.z..': '..pn..'/'..zn)
+		end
+		return
+	end
+    if args=="" then
+		local fp = Totals.fp
+		if fp then 
+			local s,fp,pv = '', tonumber(fp), 0
+			if fp>9 then
+				for p,n in pairs(Title) do
+					if fp>=p and p>pv then pv = p end
+				end
+				if pv>0 then s = ', '..Title[pv] end
+			end
+			print("Birding proficiency is "..fp..s)
+		else print("Unknown Birding proficiency.") end
+		return
+	end
+	local n,bird = args:match(nPat)
+	if n and bird then
+		n = tonumber(n)
+		for id,t  in pairs(ID) do
+			if t.n==bird then
+				local s = Zone[t.f[1]].z
+				if t.f[2] then s = s.." and "..Zone[t.f[2]].z end
+				print("The "..bird.." is found in "..s)
+				Totals[id] = n
+				print("Total sightings set to "..n)
+				return
+			end
+		end
+		print("Bird '"..bird.."' not found")
+		return
+	end
+    printe("Unknown command, "..args)
+end
+
+Turbine.Shell.AddCommand( "bl;bll;blw;bl?", BL_Command )
+
+Plugins.BirdingLog.Open = function(sender,args)
+	BL_window:SetVisible( true )
+	BL_window:SetZOrder( 2 )
+end
+
+Plugins.BirdingLog.Unload = function(sender,args)
+    Turbine.PluginData.Save(Turbine.DataScope.Server,"BL_Locs",Locs)
+    Turbine.PluginData.Save(Turbine.DataScope.Character,"BL_Totals",Totals)
+    print("Birding record saved.")
+end
+
+--~ if Event[-1] then FR_Command:Execute("fr","") end
+
+-- Options panel
+import "Vinny.Common.Options"
+OP = Vinny.Common.Options_Init(print,BL_Options,BL_window,"BL_Options")
+
+-- Help text
+help = {
+	pre = "bl",
+	arg = {
+		[""] = "Display personal birding details.",
+		[" "] = "Display birding proficiency.",
+		["# <name>"] = "Set count for <name> to #.",
+		sight = "List birding sighting records.",
+		zones = "List birding zones and counts.",
+	},
+	cmd = {
+		bll = {
+			[" "] = "Birding location commands.",
+			[";loc"] = "Set the current birding zone.",
+			list = "List sightings in the current zone.",
+			zone = "List all birds and sightings in the zone.",
+		},
+		blw = "Open Birding Log window.",
+	},
+}
---
+++ BirdingLog/BL_Window.lua
@@ -0,0 +1,192 @@
+-- Birding Log window handler
+-- coding: utf-8 '�
+
+import "Turbine.UI.Lotro"
+import "Vinny.Common"
+import "Vinny.Common.DropMenu"
+import "Vinny.Common.ToolTip"
+
+local labelFont = Turbine.UI.Lotro.Font.Verdana14
+local foreColor = Turbine.UI.Color( 0.9, 0.9, 0 )
+local whiteColor = Turbine.UI.Color( 1.0, 1.0, 1.0 )
+local backColor = Turbine.UI.Color( 0.0, 0.0, 0.0 )
+local greyColor = Turbine.UI.Color( 0.1, 0.1, 0.1 )
+local Button = Turbine.UI.Lotro.Button
+local Label = Turbine.UI.Label
+local TextBox = Turbine.UI.TextBox
+local CheckBox = Turbine.UI.Lotro.CheckBox
+local DropMenu = Vinny.Common.DropMenu
+local Item = Turbine.UI.Lotro.ShortcutType.Item
+local Hobby = Turbine.UI.Lotro.ShortcutType.Hobby
+local Alias = Turbine.UI.Lotro.ShortcutType.Alias
+local Shortcut = Turbine.UI.Lotro.Shortcut
+local Quickslot = Turbine.UI.Lotro.Quickslot
+local Qsize = 34
+local Blank
+
+BL_Window = class( Turbine.UI.Lotro.Window )
+
+function BL_Window:AddField(control, text, pos, size)
+	local field = control()
+	field:SetParent( self )
+	if text then field:SetText( text ) end
+	field:SetPosition( pos.x,pos.y )
+	field:SetSize( size.x,size.y )
+	if control==Button or control==DropMenu or not text then return field end
+	field:SetFont( labelFont )
+	field:SetForeColor( text=="" and whiteColor or foreColor )
+	local grey = control==TextBox or control==Quickslot
+	field:SetBackColor( grey and greyColor or backColor )
+	field:SetTextAlignment( Turbine.UI.ContentAlignment.MiddleLeft )
+	return field
+end
+
+function BL_Shortcut(sender,name,iname)
+	local shortcut = sender:GetShortcut()
+	local itemType = shortcut:GetType()
+	if itemType==0 then return end
+	local itemData = shortcut:GetData()
+	if sender:IsAltKeyDown() then print("Type="..itemType..", Data="..itemData) end
+	if itemType~=Item then 
+		sender:SetShortcut(Blank) 
+		print(name.." reset.")
+		return 
+	end
+	local Item = shortcut:GetItem()
+	if not Item then printe("Item is null.") return end
+	if iname and Item:GetName():sub(-#iname)~=iname then
+		printe(Item:GetName().." is not a "..iname)
+		sender:SetShortcut(Blank)
+		return
+	end
+	print(name.." set to "..Item:GetName())
+	return itemData
+end
+
+function BL_Window:Constructor()
+	Turbine.UI.Lotro.Window.Constructor( self )
+
+	-- Position the window near the top center of the screen.
+	self:SetSize( 340,250 )
+--	self:SetBackColor( Turbine.UI.Color() )
+	local pos = BL_Options.pos1 or 
+		{ x=(Turbine.UI.Display.GetWidth() - self:GetWidth())/3, 
+		y=(Turbine.UI.Display:GetHeight() - self:GetHeight())*.7 }
+	self:SetPosition( pos.x, pos.y )
+	self:SetText( "Birding Log" )
+	self:SetVisible( false )
+
+-- Hobby:Birding action is Type=Hobby(9), Data=0x7000EE1E
+
+	-- Create a Name field
+	self.name = self:AddField(Label, "Birding kit:", {x=37,y=47}, {x=80,y=16} )
+	self.name:SetFont(Turbine.UI.Lotro.Font.TrajanPro18)
+
+	-- Create an kit field
+	self.kit = self:AddField(Quickslot, nil, {x=115,y=40}, {x=Qsize,y=Qsize} )
+	Blank = self.kit:GetShortcut()
+	if Totals.kit then self.kit:SetShortcut( Shortcut(Item,Totals.kit) ) 
+	else self.kit:SetBackground("Vinny/BirdingLog/Kit.tga") end
+	self.kit.ShortcutChanged = function( sender, args )
+		Totals.kit = BL_Shortcut(sender,"Birding Kit","Birding Kit")
+	end
+
+	-- Create a birding label
+	self:AddField(Label, "Spot bird:", {x=185,y=45}, {x=70,y=16} )
+
+	-- Create an birding field
+	self.fish = self:AddField(Quickslot, nil, {x=255,y=40}, {x=Qsize,y=Qsize} )
+	self.fish:SetShortcut( Shortcut(Hobby,"0x7006B1F4") )
+    self.fish:SetAllowDrop( false )
+	self.fish.MouseEnter = function( sender, args ) Track = true end
+	self.fish.MouseLeave = function( sender, args ) Track = false end
+
+	-- Create a weapon label
+	self:AddField(Label, "Weapon:", {x=45,y=95}, {x=70,y=16} )
+
+	-- Create an weapon field, weapon slot=16, cat=104
+	self.weapon = self:AddField(Quickslot, nil, {x=115,y=90}, {x=Qsize,y=Qsize} )
+	if Totals.wpn then self.weapon:SetShortcut( Shortcut(Item,Totals.wpn) ) 
+	else self.weapon:SetBackground("Vinny/BirdingLog/Sword.tga") end
+	self.weapon.ShortcutChanged = function( sender, args )
+		Totals.wpn = BL_Shortcut(sender,"Weapon")
+	end
+
+	-- Create a Shield label
+	self:AddField(Label, "2nd slot:", {x=190,y=95}, {x=60,y=16} )
+
+	-- Create an shield field, shield slot=17
+	self.shield = self:AddField(Quickslot, nil, {x=255,y=90}, {x=Qsize,y=Qsize} )
+	if Totals.shl then self.shield:SetShortcut( Shortcut(Item,Totals.shl) ) 
+	else self.shield:SetBackground("Vinny/BirdingLog/Shield.tga") end
+	self.shield.ShortcutChanged = function( sender, args )
+		Totals.shl = BL_Shortcut(sender,"2nd")
+	end
+
+	-- Create a set location button
+	self.locButton = self:AddField(Button, "Set Zone", {x=30,y=140}, {x=125,y=20} )
+	local slot = Turbine.UI.Lotro.Quickslot()
+	slot:SetParent( self.locButton )
+    slot:SetPosition( 2,2 )
+    slot:SetSize( 99, 15 )
+    slot:SetShortcut(Turbine.UI.Lotro.Shortcut( Alias,"/bll ;loc" ))
+    slot:SetAllowDrop( false )
+
+	-- Create a Zone menu button
+	self.zoneMenu = self:AddField(DropMenu, "", {x=175,y=140}, {x=135,y=20} )
+	local action = function(args)
+		locStr = Zname[args]
+		if not Locs[locStr] then Locs[locStr] = {} end
+	end
+	self.zoneMenu.Menu.Click = function()
+		self.zoneMenu:BuildMenu(Zlist,action)
+	end
+
+	-- Create a sighting listing button
+	self.birdsButton = self:AddField(Button, "Zone Birds", {x=30,y=170}, {x=125,y=20} )
+	self.birdsButton.Click = function( sender,args )
+        BL_Command:Execute("bll","zone")
+	end
+
+	-- Create a zones button
+	self.zonesButton = self:AddField(Button, "List Zones", {x=175,y=170}, {x=135,y=20} )
+	self.zonesButton.Click = function( sender,args )
+        BL_Command:Execute("bl","zones")
+	end
+
+	-- Create a sighting listing button
+	self.birdsButton = self:AddField(Button, "Birds Seen", {x=30,y=200}, {x=125,y=20} )
+	self.birdsButton.Click = function( sender,args )
+        BL_Command:Execute("bll","list")
+	end
+
+	-- Create a totals button
+	self.totalsButton = self:AddField(Button, "Personal Totals", {x=175,y=200}, {x=135,y=20} )
+	self.totalsButton.Click = function( sender,args )
+        BL_Command:Execute("bl","sight")
+	end
+
+end
+
+function BL_Window:SetItem(id,item,xname)
+    if not item then item = Mats[id] end
+    if not xname then xname = string.format(xlink,id,item.N) end
+	self.id = id
+	self.item = item
+	self.name:SetText( item.N )
+	self.tier:SetText(item.T.." ("..Tier[item.T]..")")
+	local desc = item.P and Proc[item.P] or "Guild rep item"
+	self.type:SetText( desc )
+end
+
+BL_window = BL_Window()
+
+-- Set Escape action
+BL_window:SetWantsKeyEvents( true )
+BL_window.KeyDown = function(sender, args)
+	if( args.Action == Turbine.UI.Lotro.Action.Escape ) then
+		BL_window:SetVisible( false )
+	-- elseif Track and args.Control then 
+	-- 	BL_window.fish:MouseDown(sender, args)
+	end
+end
